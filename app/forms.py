a00d7f626d3a8390b4996f2237c3f5b90d5cee1b
NDanilowicz <nicholasdanilowicz@gmail.com>
2025-02-23 11:49:19 -0500

    -Added a searchbar to the home page for Patrons & Librarians -Bootstrap formatting now applies to Patron & Home pages

---
+++ app/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class SearchForm(forms.Form):
+    search_text = forms.CharField(label='', max_length=100, required=False)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+
+from .forms import SearchForm
 from .models import UserProfile
 
 
@@ -12,12 +14,18 @@ def home(request):
 
     try:
         user_profile = request.user.userprofile
+        form = SearchForm()
+        if request.method == 'GET':
+            form = SearchForm(request.GET)
+            if form.is_valid():
+                search_text = form.cleaned_data["search_text"]
         if user_profile.role == 'patron':
-            return render(request, 'home.html')
+            return render(request, 'home.html', {"form": form})
         else:
-            return render(request, 'librarian_home.html')
+            return render(request, 'librarian_home.html', {"form": form})
     except UserProfile.DoesNotExist:
         return redirect('select_role')
+
 
 
 def select_role(request):
