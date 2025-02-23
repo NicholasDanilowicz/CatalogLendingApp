2c8300b2262426884e3db29cf226cbe35df66479
BrandonYuan01 <spmsby11@gmail.com>
2025-02-22 23:26:09 -0500

    Adding social auth to requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,18 +101,7 @@ DATABASES = {
     'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'mysite_db',
-#         'USER': 'myuser',
-#         'PASSWORD': 'mypassword',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
