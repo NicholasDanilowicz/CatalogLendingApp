c4d3d9aff8cffeeddef17cbe8f0ea5a810269061
Brandon Istfan <brandonistfan@gmail.com>
2025-02-23 10:49:52 -0500

    fixing test file

--- app/tests.py
+++ app/tests.py
@@ -10,9 +10,3 @@ class TestUserProfile(TestCase):
         userprof.user.username = 'John Doe'
         userprof.role = 'patron'
         self.assertEqual(str(userprof), "User: John Doe Role: patron")
-    def test_account_is_not_one_year_old(self):
-        userprof = UserProfile.objects.create(user=User.objects.create())
-        userprof.creation_date = datetime.date.today()
-        self.assertFalse(userprof.age_months() >= 12)
-
-
