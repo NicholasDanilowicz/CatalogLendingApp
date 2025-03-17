ea605d47d23e1890c76c581459b0a05c458c8648
Jack Hauger <jackhauger5@gmail.com>
2025-03-17 18:58:53 -0400

    Redid CSS file structure for clarity and to make requirements change easier

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "app/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
